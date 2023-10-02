5e7c6ee451f2b387239270587bfff5786c83dbab
BlackDemon1000 <gw1_10@gmx.de>
2023-10-02 13:27:33 +0200

    first commit lolo

---
+++ build/turtlesim/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/parallels/ros2_SLAM/src/ros_tutorials/turtlesim
+# Build directory: /home/parallels/ros2_SLAM/build/turtlesim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("turtlesim__py")
